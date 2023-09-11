3b2cd4ad94fe72c548acb635f2e8364e4c9b7040
Pratyush Dwivedi <7183103-pratzyushd@users.noreply.gitlab.com>
2023-09-11 14:17:17 -0400

    Complete multiples task

---
+++ src/Multiples.java
@@ -0,0 +1,6 @@
+public class Multiples {
+    public static void main(String[] args) {
+        System.out.println("Multiples of 3 below 1000: " + 1000 / 3);
+        System.out.println("Multiples of 5 below 1000: " + 1000 / 5);
+    }
+}
